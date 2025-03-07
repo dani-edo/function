822adb0f141ecc5b7a3e3dba2ce200a14ee4f004
dani_edo <dani.edogawa.17@gmail.com>
2025-03-07 10:46:37 +0700

    returning function as value

--- main.go
+++ main.go
@@ -2,23 +2,39 @@ package main
 
 import "fmt"
 
-type trasformFn func(int) int
+type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumber := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled, tripled)
+
+	transformerFn1 := getTransformedFunction(&numbers)
+	transformerFn2 := getTransformedFunction(&moreNumber)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumber, transformerFn2)
+
+	fmt.Println(transformedNumbers, moreTransformedNumbers)
 }
 
-func transformNumbers(numbers *[]int, transform trasformFn) []int {
+func transformNumbers(numbers *[]int, transform transformFn) []int {
 	dNumbers := []int{}
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
 	}
 
 	return dNumbers
+}
+
+func getTransformedFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
 }
 
 func double(number int) int {
